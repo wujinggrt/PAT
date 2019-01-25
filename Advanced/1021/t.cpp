a02e9a5701dea26dde773110079a66fcf23d114f
wujinggrt <wujinggrt@qq.com>
2019-01-25 17:14:32 +0800

    Advanced 1021 done

--- Advanced/1021/t.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
-int main() {
-    int32_t n = 10000;
-    vector<vector<int32_t>> vec(n, vector<int32_t>(n, 0));
-    cout << vec.size() << "\n";
-}
